5f1f2d448fa1853e3b6435654fcb9285fc2f264a
noik2139 <noik2139@gmail.com>
2019-09-17 10:21:54 +0000

    nice

--- game.py
+++ game.py
@@ -1,1 +1,10 @@
-print("hallo world")
+x = 0
+y = 0
+f = ''
+while x != 3 and y!= 3:
+    f = input("You can travel: ")
+    for i in range(3,0,-1):
+        for j in range(1,4):
+             print(j,i, end = '  ')
+        print('')
+        print('')
